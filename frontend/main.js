16ee9dd9c94cd4d0a3b8c93bc282aa207a8c469a
danichang1 <danielle.c134@gmail.com>
2023-10-21 15:35:24 -0700

    working on file upload

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,21 @@
 import './style.css'
 
-function uploadFile(){
-    var formData = new FormData();
-    formData.append("filename", "syllabusFile");
-    formData.append("uploadedFile", fileInputElement.files[0]);
-    axios.post('/upload', formData).then(rest => {console.log(res)})
-}
+const myForm = document.getElementById("myForm");
+const syllFile = document.getElementById("syllFile");
+
+myForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const endpoint = "upload.php";
+    const formData = new FormData();
+
+    console.log(syllFile.files);
+
+    formData.append("syllFile", syllFile.files[0]);
+
+    fetch(endpoint, {
+        method: "post",
+        body: formData
+    }).catch(console.error);
+
+});
