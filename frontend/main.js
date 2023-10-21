3a1b436f01fb79853e5cbdb91b0575a842657994
Peter Mora-Stevens <petermora-stevens@Peters-MacBook-Air.local>
2023-10-21 16:05:27 -0700

    updated frontend

--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,21 @@
-import './style.css'
+import axios from 'axios';
 
 const myForm = document.getElementById("myForm");
 const syllFile = document.getElementById("syllFile");
+const schedule = document.getElementsByClassName("returned-schedule");
 
 myForm.addEventListener("submit", e => {
     e.preventDefault();
 
-    const endpoint = "upload.php";
     const formData = new FormData();
 
     console.log(syllFile.files);
 
     formData.append("syllFile", syllFile.files[0]);
 
-    fetch(endpoint, {
-        method: "post",
-        body: formData
-    }).catch(console.error);
-
-});
+    
+    axios.post('/process', formData).then((res) => {
+      axios.post('/parse', res.data).then((returnedSchedule) => {
+        schedule.innerHTML += `${returnedSchedule}`
+      }).catch(console.error);
+    })});
